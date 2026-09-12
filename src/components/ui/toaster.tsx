99fc828a0149ed7e142df104a829f5a71b573a92
MROFOSU <michaeltieku32@gmail.com>
2026-09-12 02:58:46 +0000

    toast change

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,24 +1,21 @@
-import { useToast } from "@/hooks/use-toast";
-import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
+import { GooeyToaster } from "goey-toast";
+import "goey-toast/styles.css";
 
 export function Toaster() {
-  const { toasts } = useToast();
-
   return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && <ToastDescription>{description}</ToastDescription>}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
-      <ToastViewport />
-    </ToastProvider>
+    <GooeyToaster
+      position="top-left"
+      gap={12}
+      offset={80}
+      expand={true}
+      closeButton="top-right"
+      richColors={true}
+      visibleToasts={3}
+      preset="spring"
+      swipeToDismiss={true}
+      closeOnEscape={true}
+      maxQueue={3}
+      showProgress={false}
+    />
   );
 }
